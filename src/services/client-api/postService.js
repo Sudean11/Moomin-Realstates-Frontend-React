c1931d68ab967b0af4b04e2aa8bae2d6831bd220
Sudin <sudean.mdrr@gmail.com>
2024-02-07 17:31:10 -0600

    saved to localstorage

--- src/services/client-api/postService.js
+++ src/services/client-api/postService.js
@@ -1,16 +1,22 @@
 import { URL_LOGIN } from "../../config/url";
 import { apiService } from "../apiService";
 
-const login = async (user) => { 
-    try{
-        let result = await apiService.post(URL_LOGIN, user);
-        localStorage.setItem('auth', result.data.accessToken);
-        return result;
-    }catch(error){
-        return error;
-    }
-}
+const login = async (user) => {
+  try {
+    let result = await apiService.post(URL_LOGIN, user);
+    localStorage.setItem("auth", result.data.accessToken);
+    const [header, payloadBase64, signature] =
+      result.data.accessToken.split(".");
+    const decodedPayload = atob(payloadBase64);
+    const payload = JSON.parse(decodedPayload);
+    localStorage.setItem("email", payload.sub);
+    console.log(payload.iat);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
 
 export const postService = {
-    login
-}
+  login,
+};

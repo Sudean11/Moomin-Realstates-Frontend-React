addd3ae9238a8a31f75e6fb9396fb9c5e1e22345
Sudin <sudean.mdrr@gmail.com>
2024-02-08 15:57:12 -0600

    fav

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: 'https://182d-2a09-bac5-74d4-4e6-00-7d-57.ngrok-free.app'
+    baseURL: 'https://aaf8-2a09-bac5-74d2-8c-00-e-2e9.ngrok-free.app'
 })
